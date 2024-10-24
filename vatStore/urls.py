d096c57cca87a306a456a830072921d217dbec21
Prabhat Rathor <ITXPRABHATZ@MacBook-Air.local>
2024-10-24 23:47:03 +0530

    Initial Update

---
+++ vatStore/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('AboutUs/', views.AboutUs, name='AboutUs'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('update_password/', views.update_password, name='update_password'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_info/', views.update_info, name='update_info'),
+    path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:foo>', views.category, name='category'),
+    path('category_summary/', views.category_summary, name='category_summary'),
+    path('search/', views.search, name='search'),
+    path('blog/', views.blog, name='blog'),
+    path('', views.main, name='main'),
+]
+
